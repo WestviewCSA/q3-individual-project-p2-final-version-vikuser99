76f6b130e05076ac6f23814b538613b30f4caac9
vikuser99 <167051399+vikuser99@users.noreply.github.com>
2026-03-21 12:42:58 -0700

    Moved my stack and queue appraoch into new file mazesolver. Fixed Filereader getText method, but need to change later and make it return 3d array.

--- Q3 Project 1 Period 2/src/FileReader.java
+++ Q3 Project 1 Period 2/src/FileReader.java
@@ -1,38 +1,39 @@
 import java.io.File;
-
 import java.io.FileNotFoundException;
 import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class FileReader {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 	
-	public Queue<String> getText(String passedFile) {
-		Queue<String> val = new ArrayDeque<>();
+	public String[][] getText(String passedFile) {
 		File file = new File(passedFile);
 		
-		String rows ="";
-		String cols = "";
 		 try {
 			 Scanner scan= new Scanner(file);
 			 scan = new Scanner(file);
-			  rows= scan.next();
-			  cols = scan.next();
-			  String levels = scan.next();
-			 while(scan.hasNext()) {
-				 String nextVal = scan.next();
-				 val.add(nextVal);
-			 }
+			  int rows= Integer.parseInt(scan.next());
+			  int cols = Integer.parseInt(scan.next());
+			  int levels = Integer.parseInt(scan.next());
+			  
+			  String[][] maze = new String[rows][cols];
+			  for(int i =0; i<rows; i++) {
+				  String line = scan.next();
+				  for(int j =0; j<cols; j++) {
+					  maze[i][j] = "" + line.charAt(j);
+				  }
+			  }
+			  return maze;
+			 
+			 
 		 } catch(FileNotFoundException e) {
 			 e.printStackTrace();  
 		 }
-		 return val;
+		 return null;
 		
 	}
 	
@@ -60,7 +61,7 @@ public class FileReader {
 				String character = scan.next();
 				int rowL = Integer.parseInt(scan.next());
 				int colL = Integer.parseInt(scan.next());
-				if(rowL <= Integer.parseInt(rows) || colL <= Integer.parseInt(columns)) {
+				if(rowL >= Integer.parseInt(rows) || colL >= Integer.parseInt(columns)) {
 					System.out.println("Coords don't match");
 					String[][]empty =new String[0][0];
 					return empty;
@@ -91,143 +92,4 @@ public class FileReader {
 	}
 
 }
-
-	
-	public void queueApproach(String [][] maze) {
-		Queue<int[]> queue1 = new ArrayDeque<>();
-		boolean [][] visited = new boolean[maze.length][maze[0].length];
-		int[][][] parent = new int[maze.length][maze[0].length][2];
-		int startRow =0;
-		int startCol =0;
-		for(int i =0; i< maze.length; i++) {
-			for(int j =0; j<maze[0].length; j++) {
-				if(maze[i][j].equals("W")) {
-					startRow = i;
-					startCol = j; // setting coords for the starting position
-				}
-			}
-		
-			
-		}
-		queue1.add(new int[] {startRow, startCol}); // will this work?
-		visited[startRow][startCol] = true;
-		
-		while(!queue1.isEmpty()) {
-			int [] current = queue1.remove();
-			int row = current[0];
-			int col = current[1];
-			
-			if (maze[row][col].equals("$")) {
-				  int[] current2 = {row, col};
-		            while(!maze[current2[0]][current2[1]].equals("W")) {
-		                maze[current2[0]][current2[1]] = "+";
-		                current2 = parent[current2[0]][current2[1]];
-		            }
-		            return;
-		        }
-		    
-			int newRow = row - 1;
-			int newCol = col;
-			if(newRow >= 0 && newCol >= 0 && newRow < maze.length && newCol < maze[0].length && !visited[newRow][newCol] && !maze[newRow][newCol].equals("@")) {
-				visited[newRow][newCol] = true;
-				parent[newRow][newCol] = new int[]{row, col};
-			    queue1.add(new int[]{newRow, newCol});
-			}
-			newRow = row + 1;
-			newCol = col;
-			if(newRow >= 0 && newCol >= 0 && newRow < maze.length && newCol < maze[0].length && !visited[newRow][newCol] && !maze[newRow][newCol].equals("@")) {
-				visited[newRow][newCol] = true;
-				parent[newRow][newCol] = new int[]{row, col};
-			    queue1.add(new int[]{newRow, newCol});
-			}
-			newRow = row;
-			newCol = col+1;
-			if(newRow >= 0 && newCol >= 0 && newRow < maze.length && newCol < maze[0].length && !visited[newRow][newCol] && !maze[newRow][newCol].equals("@")) {
-				visited[newRow][newCol] = true;
-				parent[newRow][newCol] = new int[]{row, col};
-			    queue1.add(new int[]{newRow, newCol});
-			}
-			
-			newRow = row;
-			newCol = col-1;
-			if(newRow >= 0 && newCol >= 0 && newRow < maze.length && newCol < maze[0].length && !visited[newRow][newCol] && !maze[newRow][newCol].equals("@")) {
-				visited[newRow][newCol] = true;
-				parent[newRow][newCol] = new int[]{row, col};
-			    queue1.add(new int[]{newRow, newCol});
-			}
-
-		}
-	}
-		
-		
-		
-	
-	public void stackAppoach(String [][] maze) {
-		Stack<int[]> stack = new Stack<>();
-		boolean [][] visited = new boolean[maze.length][maze[0].length];
-		int[][][] parent = new int[maze.length][maze[0].length][2];
-		int startRow =0;
-		int startCol =0;
-		for(int i =0; i< maze.length; i++) {
-			for(int j =0; j<maze[0].length; j++) {
-				if(maze[i][j].equals("W")) {
-					startRow = i;
-					startCol = j; // setting coords for the starting position
-				}
-			}
-		
-			
-		}
-		queue1.add(new int[] {startRow, startCol}); // will this work?
-		visited[startRow][startCol] = true;
-		
-		while(!stack.isEmpty()) {
-			int [] current = stack.pop();
-			int row = current[0];
-			int col = current[1];
-			
-			if (maze[row][col].equals("$")) {
-				  int[] current2 = {row, col};
-		            while(!maze[current2[0]][current2[1]].equals("W")) {
-		                maze[current2[0]][current2[1]] = "+";
-		                current2 = parent[current2[0]][current2[1]];
-		            }
-		            return;
-		        }
-		    
-			int newRow = row - 1;
-			int newCol = col;
-			if(newRow >= 0 && newCol >= 0 && newRow < maze.length && newCol < maze[0].length && !visited[newRow][newCol] && !maze[newRow][newCol].equals("@")) {
-				visited[newRow][newCol] = true;
-				parent[newRow][newCol] = new int[]{row, col};
-			    stack.push(new int[]{newRow, newCol});
-			}
-			newRow = row + 1;
-			newCol = col;
-			if(newRow >= 0 && newCol >= 0 && newRow < maze.length && newCol < maze[0].length && !visited[newRow][newCol] && !maze[newRow][newCol].equals("@")) {
-				visited[newRow][newCol] = true;
-				parent[newRow][newCol] = new int[]{row, col};
-			    stack.push(new int[]{newRow, newCol});
-			}
-			newRow = row;
-			newCol = col+1;
-			if(newRow >= 0 && newCol >= 0 && newRow < maze.length && newCol < maze[0].length && !visited[newRow][newCol] && !maze[newRow][newCol].equals("@")) {
-				visited[newRow][newCol] = true;
-				parent[newRow][newCol] = new int[]{row, col};
-			    stack.push(new int[]{newRow, newCol});
-			}
-			
-			newRow = row;
-			newCol = col-1;
-			if(newRow >= 0 && newCol >= 0 && newRow < maze.length && newCol < maze[0].length && !visited[newRow][newCol] && !maze[newRow][newCol].equals("@")) {
-				visited[newRow][newCol] = true;
-				parent[newRow][newCol] = new int[]{row, col};
-			    stack.push(new int[]{newRow, newCol});
-			}
-
-		}
-		
-		
-	}
-	
 
---
+++ Q3 Project 1 Period 2/src/MazeSolver.java
@@ -0,0 +1,141 @@
+import java.util.Queue;
+import java.util.Stack;
+
+public class MazeSolver {
+	public void queueApproach(String [][] maze) {
+		Queue<int[]> queue1 = new ArrayDeque<>();
+		boolean [][] visited = new boolean[maze.length][maze[0].length];
+		int[][][] parent = new int[maze.length][maze[0].length][2];
+		int startRow =0;
+		int startCol =0;
+		for(int i =0; i< maze.length; i++) {
+			for(int j =0; j<maze[0].length; j++) {
+				if(maze[i][j].equals("W")) {
+					startRow = i;
+					startCol = j; // setting coords for the starting position
+				}
+			}
+		
+			
+		}
+		queue1.add(new int[] {startRow, startCol}); // will this work?
+		visited[startRow][startCol] = true;
+		
+		while(!queue1.isEmpty()) {
+			int [] current = queue1.remove();
+			int row = current[0];
+			int col = current[1];
+			
+			if (maze[row][col].equals("$")) {
+				  int[] current2 = {row, col};
+		            while(!maze[current2[0]][current2[1]].equals("W")) {
+		                maze[current2[0]][current2[1]] = "+";
+		                current2 = parent[current2[0]][current2[1]];
+		            }
+		            return;
+		        }
+		    
+			int newRow = row - 1;
+			int newCol = col;
+			if(newRow >= 0 && newCol >= 0 && newRow < maze.length && newCol < maze[0].length && !visited[newRow][newCol] && !maze[newRow][newCol].equals("@")) {
+				visited[newRow][newCol] = true;
+				parent[newRow][newCol] = new int[]{row, col};
+			    queue1.add(new int[]{newRow, newCol});
+			}
+			newRow = row + 1;
+			newCol = col;
+			if(newRow >= 0 && newCol >= 0 && newRow < maze.length && newCol < maze[0].length && !visited[newRow][newCol] && !maze[newRow][newCol].equals("@")) {
+				visited[newRow][newCol] = true;
+				parent[newRow][newCol] = new int[]{row, col};
+			    queue1.add(new int[]{newRow, newCol});
+			}
+			newRow = row;
+			newCol = col+1;
+			if(newRow >= 0 && newCol >= 0 && newRow < maze.length && newCol < maze[0].length && !visited[newRow][newCol] && !maze[newRow][newCol].equals("@")) {
+				visited[newRow][newCol] = true;
+				parent[newRow][newCol] = new int[]{row, col};
+			    queue1.add(new int[]{newRow, newCol});
+			}
+			
+			newRow = row;
+			newCol = col-1;
+			if(newRow >= 0 && newCol >= 0 && newRow < maze.length && newCol < maze[0].length && !visited[newRow][newCol] && !maze[newRow][newCol].equals("@")) {
+				visited[newRow][newCol] = true;
+				parent[newRow][newCol] = new int[]{row, col};
+			    queue1.add(new int[]{newRow, newCol});
+			}
+
+		}
+	}
+		
+		
+		
+	
+	public void stackAppoach(String [][] maze) {
+		Stack<int[]> stack = new Stack<>();
+		boolean [][] visited = new boolean[maze.length][maze[0].length];
+		int[][][] parent = new int[maze.length][maze[0].length][2];
+		int startRow =0;
+		int startCol =0;
+		for(int i =0; i< maze.length; i++) {
+			for(int j =0; j<maze[0].length; j++) {
+				if(maze[i][j].equals("W")) {
+					startRow = i;
+					startCol = j; // setting coords for the starting position
+				}
+			}
+		
+			
+		}
+		stack.push(new int[] {startRow, startCol}); // will this work?
+		visited[startRow][startCol] = true;
+		
+		while(!stack.isEmpty()) {
+			int [] current = stack.pop();
+			int row = current[0];
+			int col = current[1];
+			
+			if (maze[row][col].equals("$")) {
+				  int[] current2 = {row, col};
+		            while(!maze[current2[0]][current2[1]].equals("W")) {
+		                maze[current2[0]][current2[1]] = "+";
+		                current2 = parent[current2[0]][current2[1]];
+		            }
+		            return;
+		        }
+		    
+			int newRow = row - 1;
+			int newCol = col;
+			if(newRow >= 0 && newCol >= 0 && newRow < maze.length && newCol < maze[0].length && !visited[newRow][newCol] && !maze[newRow][newCol].equals("@")) {
+				visited[newRow][newCol] = true;
+				parent[newRow][newCol] = new int[]{row, col};
+			    stack.push(new int[]{newRow, newCol});
+			}
+			newRow = row + 1;
+			newCol = col;
+			if(newRow >= 0 && newCol >= 0 && newRow < maze.length && newCol < maze[0].length && !visited[newRow][newCol] && !maze[newRow][newCol].equals("@")) {
+				visited[newRow][newCol] = true;
+				parent[newRow][newCol] = new int[]{row, col};
+			    stack.push(new int[]{newRow, newCol});
+			}
+			newRow = row;
+			newCol = col+1;
+			if(newRow >= 0 && newCol >= 0 && newRow < maze.length && newCol < maze[0].length && !visited[newRow][newCol] && !maze[newRow][newCol].equals("@")) {
+				visited[newRow][newCol] = true;
+				parent[newRow][newCol] = new int[]{row, col};
+			    stack.push(new int[]{newRow, newCol});
+			}
+			
+			newRow = row;
+			newCol = col-1;
+			if(newRow >= 0 && newCol >= 0 && newRow < maze.length && newCol < maze[0].length && !visited[newRow][newCol] && !maze[newRow][newCol].equals("@")) {
+				visited[newRow][newCol] = true;
+				parent[newRow][newCol] = new int[]{row, col};
+			    stack.push(new int[]{newRow, newCol});
+			}
+
+		}
+		
+		
+	}
+}
