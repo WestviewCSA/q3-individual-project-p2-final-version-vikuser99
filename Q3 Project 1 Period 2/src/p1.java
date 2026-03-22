d84f90e2b2c8309c3151928a3422031b6f6d4a3a
vikuser99 <167051399+vikuser99@users.noreply.github.com>
2026-03-21 22:08:18 -0700

    accidently removed file name

--- Q3 Project 1 Period 2/src/p1.java
+++ Q3 Project 1 Period 2/src/p1.java
@@ -4,6 +4,8 @@ public class p1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		String fileName = args[args.length-1];
+		
 		boolean useStack = false;
 		boolean useQueue = false;
 		boolean useOpt = false;
