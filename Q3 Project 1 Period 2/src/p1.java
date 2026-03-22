5be4ad586f33306e8c542a81779389fded98c203
vikuser99 <167051399+vikuser99@users.noreply.github.com>
2026-03-21 20:47:53 -0700

    working on command arguements...

--- Q3 Project 1 Period 2/src/p1.java
+++ Q3 Project 1 Period 2/src/p1.java
@@ -3,6 +3,26 @@ public class p1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String fileName = args[args.length-1];
+		
+		boolean useStack = false;
+		boolean useQueue = false;
+		boolean useOpt = false;
+		boolean useTime = false;
+		boolean useInCoordinate = false;
+		boolean useOutCoordinate = false;
+		
+		for(String arg: args) {
+			if(arg.equals("--Stack")) useStack = true;
+			if(arg.equals("--Queue")) useQueue = true;
+			if(arg.equals("--Opt")) useOpt = true;
+			if(arg.equals("--Time")) useStack = true;
+			if(arg.equals("--Incoordinate")) useStack = true;
+			if(arg.equals("--Outcoordinate")) useStack = true;
+			if(args.equals("--Help")) {
+				///
+			}
+		}
 		
 
 	}
